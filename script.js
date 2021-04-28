76f52e5f0d12d8723545c141a9788d797ea715b8
Jared Green <jag9292@yahoo.com>
2021-04-27 20:09:55 -0400

    first

---
+++ script.js
@@ -0,0 +1,14 @@
+var name = document.querySelector("#newName").innerHTML;
+function changeName() {
+    document.querySelector("#newName").innerHTML ="Janet Snakehole";
+}
+
+var user = document.querySelector("#removeUser").innerHTML;
+function remove() {
+    document.querySelector("#removeUser").innerHTML ="";
+}
+
+var user2 = document.querySelector("#removeUser2").innerHTML;
+function remove2() {
+    document.querySelector("#removeUser2").innerHTML ="";
+}
